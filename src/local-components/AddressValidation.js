16c5da32433d7bcda2f34a87183b2f3202864dc1
Gianluca Macciocca <gmacciocca@yuzu.com>
2016-11-18 09:12:49 -0500

    Dump USPS and use zippopotam.us for zip code validation

--- src/local-components/AddressValidation.js
+++ src/local-components/AddressValidation.js
@@ -1,36 +1,20 @@
-const USPS_USERID = "487NONE04270";
-
 export default class AddressValidation {
     constructor({ loadResource }) {
         this._loadResource = loadResource;
     }
 
     stateAndCityFromZip(zipCode) {
-        const resource = "http://production.shippingapis.com/ShippingAPI.dll?API=CityStateLookup&XML=" +
-                `<CityStateLookupRequest USERID="${USPS_USERID}"><ZipCode ID= "0"><Zip5>${zipCode}</Zip5></ZipCode></CityStateLookupRequest>`;
-        return this._loadResource.xmlResource(resource)
-            .then(xmlElement => {
-                const addressResponseBlock = this._getChildNamed(xmlElement, "ZipCode");
-                if (addressResponseBlock){
-                    const city = this._getValueOfChildNamed(addressResponseBlock, "City");
-                    const state = this._getValueOfChildNamed(addressResponseBlock, "State");
+        const resource = `https://api.zippopotam.us/us/${zipCode}`;
+        return this._loadResource.jsonResource(resource)
+            .then(response => {
+                if (response && Array.isArray(response.places) &&
+                    response.places.length){
+                    const place = response.places[0];
+                    const city = place["place name"];
+                    const state = place["state abbreviation"];
                     return city && state ? { city, state } : null;
                 }
-                return null;
+                throw Error("Invalid zip code");
             });
-    }
-
-    _getChildNamed(parent, name) {
-        const children = parent.children;
-        for (var i = 0; i < children.length; i++) {
-            if (children[i].tagName === name) {
-                return children[i];
-            }
-        }
-    }
-
-    _getValueOfChildNamed(parent, name) {
-        const child = this._getChildNamed(parent, name);
-        return (child && child.firstChild) ? child.firstChild.nodeValue : undefined;
     }
 }
---
+++ src/local-components/AddressValidation_USPS.js
@@ -0,0 +1,36 @@
+const USPS_USERID = "487NONE04270";
+
+export default class AddressValidation {
+    constructor({ loadResource }) {
+        this._loadResource = loadResource;
+    }
+
+    stateAndCityFromZip(zipCode) {
+        const resource = "http://production.shippingapis.com/ShippingAPI.dll?API=CityStateLookup&XML=" +
+                `<CityStateLookupRequest USERID="${USPS_USERID}"><ZipCode ID= "0"><Zip5>${zipCode}</Zip5></ZipCode></CityStateLookupRequest>`;
+        return this._loadResource.xmlResource(resource)
+            .then(xmlElement => {
+                const addressResponseBlock = this._getChildNamed(xmlElement, "ZipCode");
+                if (addressResponseBlock){
+                    const city = this._getValueOfChildNamed(addressResponseBlock, "City");
+                    const state = this._getValueOfChildNamed(addressResponseBlock, "State");
+                    return city && state ? { city, state } : null;
+                }
+                return null;
+            });
+    }
+
+    _getChildNamed(parent, name) {
+        const children = parent.children;
+        for (var i = 0; i < children.length; i++) {
+            if (children[i].tagName === name) {
+                return children[i];
+            }
+        }
+    }
+
+    _getValueOfChildNamed(parent, name) {
+        const child = this._getChildNamed(parent, name);
+        return (child && child.firstChild) ? child.firstChild.nodeValue : undefined;
+    }
+}
