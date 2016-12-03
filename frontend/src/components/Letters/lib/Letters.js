f0d333c9626544c181b6dc2959519263d738bd31
Gianluca Macciocca <gmacciocca@yuzu.com>
2016-12-02 23:08:02 -0500

    Auto resize paper to fit remaining screen space

--- frontend/src/components/Letters/lib/Letters.js
+++ frontend/src/components/Letters/lib/Letters.js
@@ -5,6 +5,7 @@ import { ScreenOverlay } from "../../CommonUi";
 import Topics from "./Topics";
 import AppBar from "material-ui/AppBar";
 import ContactsDrawer from "./ContactsDrawer";
+import Paper from "material-ui/Paper";
 
 export default class Letters extends React.Component {
     constructor(...args) {
@@ -15,12 +16,14 @@ export default class Letters extends React.Component {
             contacts: null,
             isContactsDrawerOpen: true,
             isSmallScreen: true,
-            showOverlay: false
+            showOverlay: false,
+            paperStyle: {}
         };
         this._offs = [];
     }
 
     componentWillMount() {
+        this.updatePaperStyle();
         const address = this._store.get("address");
         const contacts = this._store.get("contacts");
         if (address && contacts) {
@@ -29,10 +32,19 @@ export default class Letters extends React.Component {
             this.props.router.push("/");
         }
         this._offs.push(Application.roles.mediaEvents.onMediumScreen(this.onMediumScreen.bind(this)));
+        this._offs.push(Application.roles.mediaEvents.onScreenResize(this.onScreenResize.bind(this)));
+    }
+
+    componentDidMount() {
+        this.updatePaperStyle();
     }
 
     componentWillUnmount() {
         this._offs.forEach(off => off());
+    }
+
+    onScreenResize() {
+        this.updatePaperStyle();
     }
 
     onMediumScreen({ isLess }) {
@@ -45,6 +57,7 @@ export default class Letters extends React.Component {
 
     onMenuIconClick(ev) {
         ev.preventDefault();
+        ev.stopPropagation();
         const isContactsDrawerOpen = !this.state.isContactsDrawerOpen;
         this.setState({
             isContactsDrawerOpen,
@@ -52,7 +65,9 @@ export default class Letters extends React.Component {
         });
     }
 
-    onOverlayClick() {
+    onOverlayClick(ev) {
+        ev.preventDefault();
+        ev.stopPropagation();
         this.setState({
             isContactsDrawerOpen: false,
             showOverlay: false
@@ -72,9 +87,55 @@ export default class Letters extends React.Component {
             { "letters__appbar-left-spacer__showing": this.isDrawerOpenAndFixed });
     }
 
-    get appbarWrapperClass() {
-        return classnames("letters__appbar-wrapper",
-            { "letters__appbar-wrapper__shifted": this.isDrawerOpenAndFixed });
+    get appbarPaperWrapperClass() {
+        return classnames("letters__appbar-paper-wrapper",
+            { "letters__appbar-paper-wrapper__shifted": this.isDrawerOpenAndFixed });
+    }
+
+    paperRef(ref) {
+        if (this._paperRef !== ref) {
+            this._paperRef = ref;
+        }
+    }
+
+    appBarPaperWrapperRef(ref) {
+        if (this._appBarPaperWrapperRef !== ref) {
+            this._appBarPaperWrapperRef = ref;
+        }
+    }
+
+    updatePaperStyle() {
+        if (!this._paperRef || !this._appBarPaperWrapperRef) {
+            return;
+        }
+
+        const margin = 20;
+        const availableWidth = this._appBarPaperWrapperRef.offsetWidth /* - this._paperRef.offsetLeft */ - (margin * 2);
+        const availableHeight = this._appBarPaperWrapperRef.offsetHeight - this._paperRef.offsetTop - margin;
+
+        // Letter size = 215.9 by 279.4
+        // 215.9 : 279.4 = availableWidth : availableHeight
+
+        const tryWidth = (215.9 * availableHeight) / 279.4;
+        const tryHeight = (279.4 * availableWidth) / 215.9;
+
+        if (tryWidth <= availableWidth) {
+        }
+
+        // const ratio = 215.9 / 279.4;
+
+
+        const paperWidth = availableWidth;
+        const paperHeight = availableHeight;
+
+        const paperStyle = {
+            height: `${paperHeight}px`,
+            width: `${paperWidth}px`,  // `calc(100% - ${margin * 2}px)`,
+            margin: `0 ${margin}px ${margin}px`,
+            textAlign: "center",
+            display: "inline-block",
+        };
+        this.setState({ paperStyle });
     }
 
     render() {
@@ -83,9 +144,9 @@ export default class Letters extends React.Component {
         }
         return (
             <div className="letters">
-                <div className="letters__spacer-and-appbar-wrapper">
+                <div className="letters__spacer-and-appbar-paper-wrapper">
                     <div className={this.appbarSpacerClass} />
-                    <div className={this.appbarWrapperClass}>
+                    <div ref={this.appBarPaperWrapperRef.bind(this)} className={this.appbarPaperWrapperClass}>
                         <AppBar
                             title={Application.localize("welcome/title")}
                             showMenuIconButton={!this.state.isContactsDrawerOpen}
@@ -93,6 +154,9 @@ export default class Letters extends React.Component {
                         />
                         <div className="letters__topics-wrapper">
                             <Topics />
+                        </div>
+                        <div ref={this.paperRef.bind(this)} className="letters__paper-wrapper">
+                            <Paper style={this.state.paperStyle} zDepth={2} />
                         </div>
                     </div>
                 </div>
