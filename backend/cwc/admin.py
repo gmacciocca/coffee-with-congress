f09481ad25f6f388a953431c2bb99991f742312a
Senad Uka <senad.uka@gmail.com>
2016-12-18 07:51:43 +0100

    created model and admin interface

--- backend/cwc/admin.py
+++ backend/cwc/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from cwc import models
 
 # Register your models here.
+admin.site.register(models.Issue)
+admin.site.register(models.Template)
+admin.site.register(models.Contact)
+admin.site.register(models.State)
+admin.site.register(models.City)
--- backend/cwc/models.py
+++ backend/cwc/models.py
@@ -1,9 +1,63 @@
 from __future__ import unicode_literals
-
+from django.utils import timezone
 from django.db import models
+
+
+def one_day_from_now():
+    return timezone.now() + timezone.timedelta(days=1)
+
+def one_year_from_now():
+    return timezone.now() + timezone.timedelta(days=365)
 
 # Create your models here.
 class Issue(models.Model):
     issue_name = models.CharField(max_length=200)
+    until= models.DateTimeField(default=one_year_from_now)
+    def __unicode__(self):
+        return self.issue_name
 
-    
+class State(models.Model):
+    name = models.CharField(max_length=150)
+    code = models.CharField(max_length=3)
+    def __unicode__(self):
+        return self.name + ' ' + self.code
+
+class City(models.Model):
+    name = models.CharField(max_length=150)
+    state = models.ForeignKey(State, on_delete=models.CASCADE, null=True)
+    def __unicode__(self):
+        return self.name
+
+
+class Contact(models.Model):
+    name = models.CharField(max_length=200)
+    address1 = models.CharField(max_length=250)
+    address2 = models.CharField(max_length=250)
+    address3 = models.CharField(max_length=250)
+    zipcode =  models.CharField(max_length=10)
+    emails = models.CharField(max_length=500)
+    phone = models.CharField(max_length=250)
+    fax = models.CharField(max_length=250)
+
+class Location(models.Model):
+    zipcode= models.CharField(max_length=10)
+    city = models.ForeignKey(City, on_delete=models.CASCADE)
+    state = models.ForeignKey(State, on_delete=models.CASCADE)
+
+class Representation(models.Model):
+    until=  models.DateTimeField(default=one_day_from_now)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE, null=True)
+    contact = models.ForeignKey(Contact, on_delete=models.CASCADE, null=True)
+    role = models.CharField(max_length=150, default="Representative")
+    city = models.ForeignKey(City, on_delete=models.CASCADE, null=True)
+    state = models.ForeignKey(State, on_delete=models.CASCADE, null=True)
+
+
+class Template(models.Model):
+    content= models.TextField()
+    until= models.DateTimeField(default=one_year_from_now)
+    issue = models.ForeignKey(Issue, on_delete=models.CASCADE, null=True)
+    city = models.ForeignKey(City, on_delete=models.CASCADE, null=True)
+    state = models.ForeignKey(State, on_delete=models.CASCADE, null=True)
+    def __unicode__(self):
+        return self.content[:150]
