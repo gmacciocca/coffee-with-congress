8dec604d8f016f9c48aeaed0fa603f031b66053b
Senad Uka <senad.uka@gmail.com>
2016-12-21 13:29:47 +0100

    GET /issues & city optional

--- backend/cwc/views.py
+++ backend/cwc/views.py
@@ -1,5 +1,10 @@
 from django.http import JsonResponse
-from cwc.models import Issue
+from cwc import models
+from django.utils import timezone
 
 def index(request):
     return JsonResponse("Hello, cofee with congress. You are at index.")
+
+def get_issues(request):
+    issues = [issue.for_export() for issue in models.Issue.objects.filter(until__gte=timezone.now())]
+    return JsonResponse(issues, safe=False)
