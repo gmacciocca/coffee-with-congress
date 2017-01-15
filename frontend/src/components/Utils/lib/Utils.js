67fc4cb7abf5687572766fdaa7078ec7fa2c7f61
Gianluca Macciocca <gmacciocca@yuzu.com>
2017-01-14 20:40:41 -0500

    Spaces

--- frontend/src/components/Utils/lib/Utils.js
+++ frontend/src/components/Utils/lib/Utils.js
@@ -1,10 +1,13 @@
 export default class Utils {
+
     isNullOrUndefined(what) {
         return (what === undefined || what === null);
     }
+
     newLineToBr(content) {
         return content.replace(/\n/g, "<br>");
     }
+
     spaceBetween(...args){
         const strings = [...args];
         return strings.filter(str => !!str).join(" ");
