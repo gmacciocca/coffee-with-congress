8e0f18e3a8c2050e328398f0a67aab5fb7eb943b
Gianluca Macciocca <gmacciocca@yuzu.com>
2017-01-16 20:43:10 -0500

    Moved google analytics creation in Analytics class constructor. Use new config analytics flag.

--- frontend/src/components/Gateways/lib/Analytics.js
+++ frontend/src/components/Gateways/lib/Analytics.js
@@ -9,33 +9,39 @@ const LEVEL_ID = {
 export default class Analytics {
     constructor({ utils }) {
         this._utils = utils;
-        ga("set", "appName", Application.configuration.clientName);
-        ga("set", "appVersion", Application.configuration.clientVersion);
+        this._sendAnalitics = Application.configuration.analytics.google;
+        if (this._sendAnalitics) {
+            ga("create", "UA-89461481-1", "auto");
+            ga("set", "appName", Application.configuration.clientName);
+            ga("set", "appVersion", Application.configuration.clientVersion);
+        }
     }
 
     sendAppLoadedStatistics() {
-        if (Application.configuration.env !== "development") {
-            ga("send", "event", {
-                eventCategory: "application",
-                eventAction: "loaded",
-                transport: "beacon"
-            });
+        if (!this._sendAnalitics) {
+            return;
         }
+        ga("send", "event", {
+            eventCategory: "application",
+            eventAction: "loaded",
+            transport: "beacon"
+        });
     }
 
     sendPrintStatistics({ issueId, state, level }) {
-        if (Application.configuration.env !== "development") {
-            const eventCategory = this._utils.isNullOrUndefined(issueId) ? "no-issue-id" : issueId.toString();
-            const eventAction = "print";
-            const eventLabel = state;
-            const eventValue = LEVEL_ID[level];
-            ga("send", "event", {
-                eventCategory,
-                eventAction,
-                eventLabel,
-                eventValue,
-                transport: "beacon"
-            });
+        if (!this._sendAnalitics) {
+            return;
         }
+        const eventCategory = this._utils.isNullOrUndefined(issueId) ? "no-issue-id" : issueId.toString();
+        const eventAction = "print";
+        const eventLabel = state;
+        const eventValue = LEVEL_ID[level];
+        ga("send", "event", {
+            eventCategory,
+            eventAction,
+            eventLabel,
+            eventValue,
+            transport: "beacon"
+        });
     }
 }
