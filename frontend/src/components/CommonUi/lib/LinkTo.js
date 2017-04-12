7ce651483c3adfcfb8676287f4b08bd6989ccc6d
Gianluca Macciocca <gmacciocca@yuzu.com>
2017-04-11 22:07:31 -0400

    Allow passing style to LinkTo component.

--- frontend/src/components/CommonUi/lib/LinkTo.js
+++ frontend/src/components/CommonUi/lib/LinkTo.js
@@ -1,9 +1,17 @@
 import React from "react";
 
-const LinkTo = ({ url, children, refFunc, className }) => {
+const LinkTo = ({ url, children, refFunc, className, style }) => {
     const clName = (className || "") + " common-ui__link-to";
+    const props = {
+        className: clName,
+        href: url || "",
+        target: "_blank"
+    };
+    if (style) {
+        props.style = style;
+    }
     return (
-        <a ref={refFunc} className={clName} href={url} target="_blank">{children}</a>
+        <a {...props} ref={refFunc} >{children}</a>
     );
 };
 
