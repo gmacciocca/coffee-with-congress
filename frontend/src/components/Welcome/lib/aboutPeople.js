566f131ea465cd7e81910ff02bfb75ba5fcb1691
Gianluca Macciocca <gmacciocca@yuzu.com>
2017-04-17 00:01:08 -0400

    Update Tanuja’s bio.

--- frontend/src/components/Welcome/lib/aboutPeople.js
+++ frontend/src/components/Welcome/lib/aboutPeople.js
@@ -8,15 +8,13 @@ const AboutText = () => (
 
 const TanujaBio = () => (
 <div>
-    Tanuja has been with Google for six years, serving as a program manager on the Docs engineering team.
-    Prior to Google, she was a Product Manager at Barnes &amp; Noble.com and Broadway.com.
-    Parallel to her 16+ year career in ecommerce and software development, Tanuja tutors math through&nbsp;
+    Tanuja has worked in the software and e-commerce development space for 16+ years with companies like InRhythm,
+    Google, Barnes & Noble.com and Broadway.com.  In parallel, she tutors math with&nbsp;
     <LinkTo url="http://www.tophonors.org/">Top Honors</LinkTo>
-    &nbsp;for 7 years and counting.
-    In 2013, she started a complementary middle school reading program&nbsp;
-    <LinkTo url="http://www.booksandbreakfast.org/">Books &amp; Breakfast</LinkTo>
-    &nbsp;in her neighborhood.
-    She worked extensively with the Hurricane Sandy recovery efforts and organized an initiative to help hundreds of local residents restore their homes from mold and water damage.
+    &nbsp;and sits on the board of the&nbsp;
+    <LinkTo url="http://www.cvtcnyc.org/">Crime Victims Treatment Center</LinkTo>.
+    &nbsp;She worked extensively with the Hurricane Sandy recovery efforts and
+    organized an initiative to help hundreds of local residents restore their homes from mold and water damage.
     Born to immigrants from the India, Tanuja earned her Bachelor&#39;s of Science from New York University.
     At 20, she launched her first commercial website and never looked back.
 </div>);
