51f7b95eda1f5e8795569da8549db36dd21808e1
Gianluca Macciocca <gmacciocca@yuzu.com>
2017-04-20 12:18:47 -0400

    Dump CSS columns and just set percentage width.

--- frontend/src/components/Dashboard/lib/TemplateSources.js
+++ frontend/src/components/Dashboard/lib/TemplateSources.js
@@ -27,35 +27,21 @@ export default class TemplateSources extends React.Component {
         const style = shouldShow ? {} : { display: "none" };
         const sourceCount = shouldShow ? sources.length : 0;
         const columnsToShow =
-            sourceCount <= 2 ? 0 : // no columns required for 1 or 2 sources
+            sourceCount === 1 ? 1 :
             sourceCount <= 4 ? 2 :
             sourceCount <= 6 ? 3 :
             sourceCount <= 8 ? 4 :
             5;
 
-        const sourcesClassname = classnames(
-            "dashboard__template-sources__list",
-            {
-                "dashboard__template-sources__list__two-columns": 2 === columnsToShow,
-                "dashboard__template-sources__list__three-columns": 3 === columnsToShow,
-                "dashboard__template-sources__list__four-columns": 4 === columnsToShow,
-                "dashboard__template-sources__list__five-columns": 5 === columnsToShow
-            });
-
-        const linkClassname = classnames(
-            "dashboard__template-sources__list__link",
-            {
-                "dashboard__template-sources__list__link__width-50": 2 === sourceCount,
-                "dashboard__template-sources__list__link__width-100": 2 !== sourceCount
-            });
+        const linkStyle = { width: `${100 / columnsToShow}%` };
 
         return (
             <div ref={refFunc} className="dashboard__template-sources-wrapper" style={style}>
                 <div ref={refFunc} className="dashboard__template-sources" >
                     <label>{Application.localize("dashboard/sources")}</label>
-                    <div className={sourcesClassname}>
+                    <div className="dashboard__template-sources__list">
                         {shouldShow && sources.map(source => {
-                            return <LinkTo className={linkClassname} key={source.title} url={source.url}>{source.title}</LinkTo>;
+                            return <LinkTo style={linkStyle} className="dashboard__template-sources__list__link" key={source.title} url={source.url}>{source.title}</LinkTo>;
                         })}
                     </div>
                 </div>
